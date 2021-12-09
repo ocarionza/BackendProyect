cb3c676aaef05dcce98fec9ad8acb5fedce81fe6
Brayan Ocampo <brayan.ocampo.c7@gmail.com>
2021-12-09 18:10:42 -0500

    Se agrega modulo de autenticacion vue --auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-import vuetify from './vuetify';
-
-
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -32,5 +29,4 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-    vuetify
 });
